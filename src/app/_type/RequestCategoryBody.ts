dcba39856a73048cb8fd0d38da91d0a77116ad15
s-yamauchi07 <hand1nhand306@gmail.com>
2024-09-28 12:31:15 +0900

    管理者_カテゴリー新規作成のAPI作成

---
+++ src/app/_type/RequestCategoryBody.ts
@@ -0,0 +1,3 @@
+export interface RequestCategoryBody{
+  name: string,
+}
--- src/app/api/admin/categories/route.ts
+++ src/app/api/admin/categories/route.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import { RequestCategoryBody } from "../../../_type/RequestCategoryBody";
 
 const prisma = new PrismaClient
 
@@ -22,3 +23,22 @@ export const GET = async (request: NextRequest) => {
       return NextResponse.json({ status: error.message }, { status: 400})
   } 
 };
+
+// カテゴリーの新規作成
+export const POST = async (request: NextRequest) => {
+  const body = await request.json()
+  const { name } = body
+
+  try {
+    const data = await prisma.category.create({
+      data: {
+        name
+      },
+    })
+
+    return NextResponse.json({ status: 'OK', message: '作成しました'}, { status: 200 })
+  } catch(error) {
+    if (error instanceof Error)
+      return NextResponse.json({ status: error.message }, { status: 400 })
+  }
+}
