d7f8cd6b7125d66778896e835d750e9d47c13eee
s-yamauchi07 <hand1nhand306@gmail.com>
2024-09-28 12:48:27 +0900

    管理者_カテゴリーの更新APIの作成

--- src/app/api/admin/categories/[id]/route.ts
+++ src/app/api/admin/categories/[id]/route.ts
@@ -1,3 +1,4 @@
+import { RequestCategoryBody } from "@/app/_type/RequestCategoryBody";
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -15,6 +16,29 @@ export const GET = async (request: NextRequest, { params}: { params: { id: strin
     })
 
     return NextResponse.json({ status: 'OK', category: category}, { status: 200 })
+  } catch(error) {
+    if (error instanceof Error)
+      return NextResponse.json({ status: error.message }, { status: 400 })
+  }
+}
+
+// カテゴリー編集のAPI
+export const PUT = async (request: NextRequest, { params}: { params: { id: string }}) => {
+  const { id } = params
+  const body = await request.json()
+  const { name }: RequestCategoryBody = body
+
+  try {
+    const category = await prisma.category.update({
+      where: {
+        id: parseInt(id)
+      },
+      data: {
+        name
+      },
+    })
+
+    return NextResponse.json({ status: 'OK', category: category }, { status: 200 })
   } catch(error) {
     if (error instanceof Error)
       return NextResponse.json({ status: error.message }, { status: 400 })
