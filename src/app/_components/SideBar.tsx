0f72f1fa9634d9d6093bbee55ee528319798ca24
s-yamauchi07 <hand1nhand306@gmail.com>
2024-09-29 23:52:25 +0900

    サイドバーの作成

---
+++ src/app/_components/SideBar.tsx
@@ -0,0 +1,11 @@
+
+const SideBar: React.FC = () => {
+  return(
+    <div className="bg-gray-200 w-80 h-screen">
+      <div className="m-4">記事一覧</div>
+      <div className="m-4">カテゴリー一覧</div>
+    </div>
+  )
+}
+
+export default SideBar;
