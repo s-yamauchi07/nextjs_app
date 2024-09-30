8b541f3d0ca932a0cbd72029a1768ac2a5a4fd0e
s-yamauchi07 <hand1nhand306@gmail.com>
2024-09-30 17:01:12 +0900

    カテゴリー一覧のフロントエンド実装

---
+++ src/app/admin/categories/page.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import SideBar from "@/app/_components/SideBar";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+type Category = {
+  id: string
+  name: string
+}
+
+const AllCategories: React.FC = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    const AllCategories = async () => {
+      try {
+        const response = await fetch("/api/admin/categories")
+        const { categories } = await response.json();
+        setCategories(categories)
+      } catch(error) {
+        console.log(error)
+      }
+    }
+    AllCategories();
+  }, []);
+
+  return(
+    <div className="flex">
+      <SideBar />
+      <div className="p-10 w-full">
+        <div className="flex justify-between items-center mb-6">
+          <h2 className="text-xl font-bold">
+            カテゴリー作成
+          </h2>
+          <div>
+            <button type="button"
+              className="bg-blue-600 text-white font-bold rounded-lg px-4 py-2"
+            >
+              <Link href="/admin/posts/new">新規作成</Link>
+            </button>
+          </div>
+        </div>
+
+        <div className="p-4">
+          <ul>
+            {categories.map((category) => {
+              return(
+                <li 
+                  key={category.id}
+                  className="font-bold text-lg py-2 border-b-2"
+                >
+                  {category.name}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default AllCategories;
