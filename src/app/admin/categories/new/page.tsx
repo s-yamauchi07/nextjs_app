4e100254185d5ebfc1675ebd354c3aa8e6339f5a
s-yamauchi07 <hand1nhand306@gmail.com>
2024-09-30 09:34:52 +0900

    カテゴリー新規作成のフロントエンド実装

---
+++ src/app/admin/categories/new/page.tsx
@@ -0,0 +1,58 @@
+"use client"
+
+import SideBar from "@/app/_components/SideBar";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Category = {
+  name: string
+}
+
+const AddCategory: React.FC = () => {
+  const { register, handleSubmit, reset } = useForm<Category>();
+
+  const onsubmit: SubmitHandler<Category> = async (data) => {
+    try {
+      await fetch("/api/admin/categories", {
+        method: "POST",
+        body: JSON.stringify(data),
+      })
+      reset()
+    } catch(error) {
+      console.log(error)
+      alert('カテゴリー投稿に失敗しました');
+    }
+  }
+
+  return(
+    <div className="flex">
+      <SideBar />
+      <div className="p-10 w-full">
+        <h2 className="text-xl font-bold mb-6">
+          カテゴリー作成
+        </h2>
+
+        <form
+          onSubmit={handleSubmit(onsubmit)}
+          className="flex flex-col gap-4"
+        >
+          <label htmlFor="name">カテゴリー</label>
+          <input type="type"
+            id="name"
+            className="border border-solid border-gray-200 rounded-lg p-2"
+            {...register("name")}
+          />
+          <div>
+            <button 
+              type="submit"
+              className="bg-blue-600 text-white font-bold rounded-lg px-4 py-2"
+            >
+              作成
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default AddCategory;
