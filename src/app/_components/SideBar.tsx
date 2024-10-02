dd6e3a8a8feb8c5f1bc7d10b4f576b6896ea28e6
s-yamauchi07 <hand1nhand306@gmail.com>
2024-10-02 14:28:04 +0900

    サイドバーへリンクの設定

--- src/app/_components/SideBar.tsx
+++ src/app/_components/SideBar.tsx
@@ -1,9 +1,27 @@
 
+import { usePathname } from 'next/navigation'
+import Link from "next/link";
+
 const SideBar: React.FC = () => {
+  const pathname = usePathname();
+  
+  const isSelected = (url: string) => {
+    if (pathname === url || pathname.startsWith(url + "/")) {
+      return true;
+    }
+    return false;
+  };
+
   return(
     <div className="bg-gray-200 w-80 h-screen">
-      <div className="m-4">記事一覧</div>
-      <div className="m-4">カテゴリー一覧</div>
+      <ul>
+        <li className={`p-4 ${isSelected("/admin/posts") ? 'bg-blue-100' : ''} `}>
+          <Link href="/admin/posts">記事一覧</Link>
+        </li>
+        <li className={`p-4 ${isSelected("/admin/categories") ? 'bg-blue-100' : ''} `}>
+          <Link href="/admin/categories">カテゴリー一覧</Link>
+        </li>
+      </ul>
     </div>
   )
 }
