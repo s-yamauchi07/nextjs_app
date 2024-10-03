980478601f5d53e7c000038e43558613a91d7733
s-yamauchi07 <hand1nhand306@gmail.com>
2024-10-03 21:33:57 +0900

    MUIの処理切り出し

--- src/utils/categoriesUtils.ts
+++ src/utils/categoriesUtils.ts
@@ -1,5 +1,5 @@
 import { Theme } from '@mui/material/styles';
-import { PostRequestCategoryBody } from "@/app/_type/PostRequestCategoryBody";
+import { RequestCategoryBody } from "@/app/_type/RequestCategoryBody";
 
 export const ITEM_HEIGHT = 48;
 export const ITEM_PADDING_TOP = 8;
@@ -12,7 +12,7 @@ export const MenuProps = {
   },
 };
 
-export function getStyles(category: PostRequestCategoryBody, categoryName: string[], theme: Theme) {
+export function getStyles(category: RequestCategoryBody, categoryName: string[], theme: Theme) {
   return {
     fontWeight: categoryName.includes(category.name)
       ? theme.typography.fontWeightMedium
