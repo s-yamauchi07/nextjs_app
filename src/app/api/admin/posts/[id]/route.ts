f03ccce2aa198ef48e5ea4225d71615bbcd75630
s-yamauchi07 <hand1nhand306@gmail.com>
2024-10-07 00:28:05 +0900

    import文の修正

--- src/app/api/admin/posts/[id]/route.ts
+++ src/app/api/admin/posts/[id]/route.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import useSupabaseSession from "@/app/_hooks/useSupabaseSession";
 import { supabase } from "@/utils/supabase";
 
 const prisma = new PrismaClient();
